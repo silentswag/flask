3aa1d85956e0384ac8ac99828ad96c85aabe3ca8
AnushreeKumar <112722298+silentswag@users.noreply.github.com>
2024-04-14 16:24:55 +0530

    basic,routes,url redirect modify url adress

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
+
 app=Flask(__name__) #variable, module
 
 
 @app.route("/") # goto diff pages- route decorators "/" represents root page
-def hello():
-    return "Hello"
+def home():
+    return "Hello world <h1> FLASK</h>"
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("user",name="Anushree"))
+
+if __name__=="__main__":
+    app.run()
