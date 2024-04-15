ed531193d3ac3f1deb2604090fc8a015994930c2
AnushreeKumar <112722298+silentswag@users.noreply.github.com>
2024-04-15 20:32:08 +0530

    basic get post

--- flaskblog.py
+++ flaskblog.py
@@ -3,10 +3,22 @@ from flask import Flask,redirect,url_for,render_template
 app=Flask(__name__) #variable, module
 
 
-@app.route("/<name>") # goto diff pages- route decorators "/" represents root page
-def home(name):
-    return render_template("index.html",content=name)
+@app.route("/") # goto diff pages- route decorators "/" represents root page
+def home():
+    return render_template("index.html")
+
+@app.route("/inherit") 
+def test_inherit():
+    return render_template("textinherit.html")
+
+@app.route("/login",methods=["POST","GET"])
+def login():
+    return render_template("login.html")
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
