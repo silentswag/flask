8c9fbecec1bef4d7a4b1b609e2bf1c7e13b7a4be
AnushreeKumar <112722298+silentswag@users.noreply.github.com>
2024-04-15 20:32:23 +0530

    GET post usage

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask,redirect,url_for,render_template
+from flask import Flask,redirect,url_for,render_template,request
 
 app=Flask(__name__) #variable, module
 
@@ -13,11 +13,15 @@ def test_inherit():
 
 @app.route("/login",methods=["POST","GET"])
 def login():
-    return render_template("login.html")
+    if request.method=="POST":
+        user=request.form['nm']
+        return redirect(url_for("user",usr=user))
+    else:
+        return render_template("login.html")
 
 @app.route("/<usr>")
 def user(usr):
-    return f"<h1>{usr}</h1>"
+    return f"<h1><b>{usr}</b</h1>"
 
 
 if __name__=="__main__":
